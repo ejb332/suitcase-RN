defdada47daccaafc3b7b03ccec542c1668003e5
Eric Biel <eric.j.biel@gmail.com>
2017-09-14 15:48:35 -0500

    Refactor index

--- client/app/index.ios.js
+++ client/app/index.ios.js
@@ -1,35 +1,4 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import configureStore from './src/store/configure-store';
-import { Provider } from 'react-redux';
-import AppContainer from './src/components/App';
-
-const store = configureStore();
-
-export default class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <View style={{ flexGrow: 1, borderWidth: 2 }}>
-          <AppContainer></AppContainer>
-        </View>
-      </Provider>
-    );
-  }
-}
-
-const styles = {
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
-};
+import { AppRegistry } from 'react-native';
+import App from './src/components/App';
 
 AppRegistry.registerComponent('app', () => App);
