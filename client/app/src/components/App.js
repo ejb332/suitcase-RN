6649074d1933b5d55448a65e6267a24a9273d858
Eric Biel <eric.j.biel@gmail.com>
2017-09-14 16:10:04 -0500

    Refactor

--- client/app/src/components/App.js
+++ client/app/src/components/App.js
@@ -1,31 +1,18 @@
 import React, { Component } from 'react';
-import { Router, Scene, Stack } from 'react-native-router-flux';
-import { View, Text, StyleSheet } from 'react-native';
-import Login from './auth/Login';
-import Signup from './auth/Signup';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import Router from './Router';
+import reducers from '../reducers';
 
 export default class App extends Component {
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <View style={styles.container}>
-        <Router>
-          <Scene key="root">
-            <Scene key="login" component={Login} title="Log In" initial />
-            <Scene key="signup" component={Signup} title="Sign Up" />
-          </Scene>
-        </Router>
-      </View>
+      <Provider store={store}>
+        <Router />
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexGrow: 1,
-    borderWidth: 5,
-    borderColor: 'blue',
-    backgroundColor: '#F5FCFF',
-    paddingTop: 40
-  }
-});
