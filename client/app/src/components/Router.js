832c1be6f8335048acd68f62e1e59d95239ef966
Eric Biel <eric.j.biel@gmail.com>
2017-09-14 16:18:35 -0500

    Add scene

--- client/app/src/components/Router.js
+++ client/app/src/components/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import Login from '../components/auth/Login';
+import Signup from '../components/auth/Signup';
 
 const RouterComponent = () => {
   return (
   <Router sceneStyle={{ paddingTop: 60 }}>
     <Scene key="auth">
-      <Scene key="login" component={Login} title="Log In" />
+      <Scene key="login" component={Login} title="Log In" initial />
+      <Scene key="signup" component={Signup} title="Sign Up" />
     </Scene>
   </Router>
   );
