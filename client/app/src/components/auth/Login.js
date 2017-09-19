72e74f4161d511f80297b73f87893833ba52a0f8
Eric Biel <eric.j.biel@gmail.com>
2017-09-19 17:58:15 -0500

    Add image to log in page

--- client/app/src/components/auth/Login.js
+++ client/app/src/components/auth/Login.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  TouchableWithoutFeedback,
-  TextInput
-} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { connect } from 'react-redux';
-import { Actions } from 'react-native-router-flux';
-import { bindActionCreators }  from 'redux';
-import login from '../../actions/auth/login';
+import { bindActionCreators } from 'redux';
 import { Spinner, Button } from '../common';
+import login from '../../actions/auth/login';
 
 class Login extends Component {
+  // these props are coming from login
   constructor(props) {
     super(props);
     this.state = {};
@@ -24,56 +18,80 @@ class Login extends Component {
     this.props.login();
   }
 
-  // renderButton() {
-  //   if (this.props.loading) {
-  //     return <Spinner size="large" />;
-  //   }
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
 
-  //   return (
-  //     <Button onPress={this.onPress.bind(this)}>
-  //       Log In
-  //     </Button>
-  //   );
-  // }
+    return (
+      <Button onPress={this.onPress.bind(this)}>
+        Log In
+      </Button>
+    );
+  }
 
   render() {
     console.log('login render state', this.state);
     return (
-      <View 
-        style={{
-          display: 'flex', 
-          flex: 1,
-          borderWidth: 1,
-          flexDirection: 'column', 
-          justifyContent: 'center', 
-          alignItems: 'center'
-        }}
-      >
+      <View style={styles.loginStyle}>
+        <Image
+        style={styles.imageStyle}
+        source={require('../../../images/Suitecase_Green_Vert.png')}
+        />
         <TextInput
+          style={styles.textStyle}
           autoCapitalize="none"
           keyboardType="email-address"
-          onChangeText={(text) => this.setState({ email: text })}
+          onChangeText={(email) => this.setState({ email })}
           placeholder="Email"
-          value={this.state.email}
+          value={this.props.email}
         />
         <TextInput
+          style={styles.textStyle}
           autoCapitalize="none"
           secureTextEntry
-          onChangeText={(text) => this.setState({ password: text })}
+          onChangeText={(password) => this.setState({ password })}
           placeholder="Password"
-          value={this.state.password}
+          value={this.props.password}
         />
-        <TouchableOpacity onPress={this.onPress}>
-          <Text>Log In</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress={() => Actions.push('signup')}>
-          <Text>Not A User? Sign Up!</Text>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={this.onPress}
+        >
+          <Text style={styles.textStyle}>Log In</Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = {
+  loginStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#35A8D9',
+  },
+  textStyle: {
+    color: '#30353B',
+    fontSize: 20,
+    textAlign: 'center',
+    width: 200
+  },
+  imageStyle: {
+    marginBottom: 25
+  },
+  buttonStyle: {
+    width: 250,
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderWidth: 1,
+    borderRadius: 4,
+    backgroundColor: '#F3F3F4',
+  }
+};
 
 export default connect(state => {
     console.log('login state', state);
