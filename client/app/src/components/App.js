bf6865cbeb4844c604133d05818cfeffd8438d3e
Eric Biel <eric.j.biel@gmail.com>
2017-09-19 17:59:05 -0500

    Correct store

--- client/app/src/components/App.js
+++ client/app/src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import ReduxThunk from 'redux-thunk';
+import configureStore from '../store/configure-store';
 import Router from './Router';
-import reducers from '../reducers';
 
 export default class App extends Component {
   render() {
-    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    const store = configureStore();
 
     return (
       <Provider store={store}>
